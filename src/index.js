b6d1e4e64975f81ea1f0ab0a53d31e326e6d2a78
Justin Palmer <encytemedia@gmail.com>
2020-12-04 09:45:12 -0800

    render notes on strings

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Note, Scale, Range } from '@tonaljs/tonal'
 import { select, range, scaleBand, scalePoint } from 'd3'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const margin = { t: 0, r: 0, b: 0, l: 0 }
   const width = window.innerWidth
   const height = Math.round(width / 6)
   const fretHeight = Math.round(height * 0.9)
@@ -11,8 +12,15 @@ document.addEventListener('DOMContentLoaded', () => {
     Range.chromatic([`${t}0`, `${t}2`], { flats: true })
   )
 
+  const fretData = frets.map((d) => {
+    return tuning.map((_, i) => notes[i][d])
+  })
+
   const fretX = scaleBand().domain(frets).rangeRound([0, width]).padding(0.05)
-  const noteY = scalePoint().domain(tuning).rangeRound([fretHeight, 0])
+  const noteY = scalePoint()
+    .padding(1)
+    .domain(range(tuning.length))
+    .rangeRound([fretHeight, 0])
 
   const svg = select('#neck')
     .append('svg')
@@ -24,6 +32,16 @@ document.addEventListener('DOMContentLoaded', () => {
     .join('g')
     .attr('transform', (d) => `translate(${fretX(d)}, 0)`)
     .attr('fill', '#eee')
+    .attr('class', 'fret')
+
+  const opens = svg
+    .selectAll('.open')
+    .data(tuning)
+    .join('g')
+    .attr('transform', (d, i) => `translate(0, ${noteY(i) + 4})`)
+    .attr('class', 'open')
+
+  opens.append('text').text(String).attr('font-size', 12)
 
   fret
     .append('rect')
@@ -45,5 +63,18 @@ document.addEventListener('DOMContentLoaded', () => {
     .attr('font-size', 50)
     .attr('fill', 'gold')
 
-  fret
+  const note = fret
+    .selectAll('.string')
+    .data(tuning)
+    .join('g')
+    .attr('transform', (d, i) => `translate(0, ${noteY(i)})`)
+    .attr('class', 'string')
+
+  note
+    .append('rect')
+    .attr('width', fretX.bandwidth())
+    .attr('height', 2)
+    .attr('fill', '#ddd')
+
+  note.append('text')
 })
