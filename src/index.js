2468f2e7eeb1f83fffc886242a87938a53c4ad8b
Justin Palmer <encytemedia@gmail.com>
2021-03-16 19:12:08 -0700

    spike out simple stuff

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-export default class FretBoard {
-  get tuning() {
-    return this.tuning
-  }
+import { Note, Scale, ScaleType, Range, Mode } from "@tonaljs/tonal";
+export { default as tnps } from "./system/tnps";
+export { default as utils } from "./utils";
+export { default as caged } from "./system/caged";
+export { default as mode } from "./system/mode";
 
-  set tuning(value) {
-    this.tuning = value
-  }
+export function frets(
+  tuning = ["E2", "A2", "D3", "G3", "B3", "E4"],
+  octaves = 2
+) {
+  const count = octaves * 12;
+
+  const notes = tuning.map((t) => {
+    const { oct, letter } = Note.get(t);
+    return Range.chromatic([t, `${letter}${oct + 2}`]).map(Note.get);
+  });
+  function fb() {}
+
+  fb.notes = () => notes;
+  fb.tuning = () => tuning;
+  fb.count = () => count;
+  fb.octaves = () => octaves;
+
+  return fb;
 }
-// export default function () {
-//   function scale() {}
-
-//   scale.tuning = function (tuning) {
-//     return arguments.length ? tuning : 'EADGBE'
-//   }
-
-//   scale.frets = function (frets) {
-//     return arguments.length ? frets : 24
-//   }
-
-//   return scale
-// }
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,40 @@
-import FretBoard from '../'
+import { Note, Scale, ScaleType, Range, Mode } from "@tonaljs/tonal";
+import { frets } from "./";
+import { assert } from "chai";
 
-describe('some test', () => {
-  it('should do something', () => {})
-})
+// {
+//   empty: false,
+//   acc: '',
+//   alt: 0,
+//   chroma: 4,
+//   coord: [ 4, 0 ],
+//   freq: 82.40688922821748,
+//   height: 40,
+//   letter: 'E',
+//   midi: 40,
+//   name: 'E2',
+//   oct: 2,
+//   pc: 'E',
+//   step: 2
+// }
+
+describe("some test", () => {
+  it("should initialize with standard tuning", () => {
+    const fb = frets();
+    assert.deepEqual(fb.tuning(), ["E2", "A2", "D3", "G3", "B3", "E4"]);
+    assert.equal(fb.count(), 24);
+    assert.equal(fb.octaves(), 2);
+  });
+
+  it("should create two octaves of notes data when initialized", () => {
+    const fb = frets();
+    const notes = fb.notes();
+    assert.isArray(notes);
+
+    fb.tuning().forEach((note, i) => {
+      const { letter, oct } = Note.get(note);
+      assert.equal(notes[i][0].name, note);
+      assert.equal(notes[i][notes[i].length - 1].name, `${letter}${oct + 2}`);
+    });
+  });
+});
