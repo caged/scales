3a648015e6a7c7cbcde813c74dd75237e3cf5f35
Justin Palmer <encytemedia@gmail.com>
2021-03-17 16:57:29 -0700

    basic working version of tnps

--- src/system/tnps.js
+++ src/system/tnps.js
@@ -1,80 +1,60 @@
-import { zip } from "d3";
+import { partition } from "../utils";
 
-export default function tnps(notes, scale) {
+export default function tnps(strings, scale) {
+  strings = [...strings];
   const numPositions = 7;
-  const positions = getPositions();
+  const notesPerString = 3;
+
   function tnps() {}
 
-  tnps.positions = () => positions;
+  const intervals = scale.intervals();
+  const snotes = scale.notes();
+  const scaleNotes = snotes.map((note, i) => {
+    return {
+      note,
+      interval: intervals[i],
+      name: note.name,
+    };
+  });
 
-  function getPositions() {
-    const intervals = scale.intervals();
-    const snotes = scale.notes();
-    const scaleNotes = snotes.map((n, i) => {
-      return {
-        note: n,
-        interval: intervals[i],
-      };
-    });
+  const forString = (index) => {
+    return partition(
+      scaleNotes,
+      notesPerString,
+      numPositions,
+      index * notesPerString
+    );
+  };
 
-    let scaleNoteIndex = 0;
-
-    // const out = [];
-    // for (let i = 1; i <= numPositions; i++) {
-    //   const notes = [];
-    //   out.push({
-    //     number: i,
-    //     notes: [],
-    //   });
-    // }
-
-    return scaleNotes;
+  let i = 0;
+  for (const [strnum, str] of strings.entries()) {
+    const strScalePositions = forString(strnum);
+    console.log("\n");
+    console.log(str[0].name, "String");
+    for (const semitone of str) {
+      semitone.positions = Date.now();
+      semitone.i = i;
+      semitone.positions = strScalePositions
+        .map((ssp, spidx) => {
+          const pos = ssp.some((n) => {
+            return n.note.pc === semitone.pc;
+          })
+            ? spidx + 1
+            : -1;
+          return pos;
+        })
+        .filter((spidx) => spidx !== -1);
+      if (strnum === 0)
+        console.log(semitone.name, semitone.pc, semitone.positions);
+      const snote = scaleNotes.find((sn) => semitone.pc === sn.note.pc);
+      if (snote) {
+        semitone.interval = snote.interval;
+      }
+    }
+    i++;
   }
 
-  return tnps;
+  console.log(strings[0][10]);
+
+  return strings;
 }
-
-// const getAtPosition = (items, strings, scale, position) => {
-//   const out = []
-//   const itemset = utils.rotate(scale[items], position)
-//   for (let string = 0; string < strings; string++) {
-//     out.push(utils.chunk(itemset, (string * 3) % itemset.length, 3))
-//   }
-//   return out
-// }
-
-// // const tuning = ['E', 'A', 'D', 'G', 'B', 'E']
-// // const scale = Scale.get('minor')
-// // const positions = 7
-// //
-// // strings - number of strings
-// // scale - A tonal Scale object
-// // position - A number 1..7 representing which 3nps scale position
-// const getIntervalsAtPosition = (strings, scale, position) => {
-//   const intervals = []
-//   const intset = utils.rotate(scale.intervals, position)
-//   for (let string = 0; string < strings; string++) {
-//     intervals.push(utils.chunk(intset, (string * 3) % intset.length, 3))
-//   }
-//   return intervals
-// }
-
-// //
-// // strings - number of strings
-// // scale - A tonal Scale object
-// // position - A number 1..7 representing which 3nps scale position
-// const getNotesAtPosition = (strings, scale, position) => {
-//   const notes = []
-//   const noteset = utils.rotate(scale.notes, position)
-//   for (let string = 0; string < strings; string++) {
-//     notes.push(utils.chunk(noteset, (string * 3) % noteset.length, 3))
-//   }
-//   return notes
-// }
-
-// export default { getIntervalsAtPosition, getNotesAtPosition }
-
-// // for (let position = 0; position < positions; position++) {
-// //   // console.log(getIntervalsAtPosition(6, scale, position))
-// //   console.log(getNotesAtPosition(6, Scale.get('D minor'), position))
-// // }
--- src/system/tnps.test.js
+++ src/system/tnps.test.js
@@ -3,10 +3,61 @@ import { frets, scale } from "../";
 import { assert } from "chai";
 
 describe("three note per string tests", () => {
+  let fb;
+  let strings;
+  let dms;
+  let notes;
+
+  beforeEach(() => {
+    fb = frets();
+    notes = fb.notes();
+    dms = scale("D minor");
+    strings = tnps(notes, dms);
+  });
+
+  // it("gets tnps for string at index", () => {
+  //   const notesForEPositions = mode
+  //     .forString(0)
+  //     .map((p) => p.map((n) => n.name));
+
+  //   const notesForAPositions = mode
+  //     .forString(1)
+  //     .map((p) => p.map((n) => n.name));
+
+  //   assert.deepEqual(notesForEPositions, [
+  //     ["D", "E", "F"],
+  //     ["E", "F", "G"],
+  //     ["F", "G", "A"],
+  //     ["G", "A", "Bb"],
+  //     ["A", "Bb", "C"],
+  //     ["Bb", "C", "D"],
+  //     ["C", "D", "E"],
+  //   ]);
+
+  //   assert.deepEqual(notesForAPositions, [
+  //     ["G", "A", "Bb"],
+  //     ["A", "Bb", "C"],
+  //     ["Bb", "C", "D"],
+  //     ["C", "D", "E"],
+  //     ["D", "E", "F"],
+  //     ["E", "F", "G"],
+  //     ["F", "G", "A"],
+  //   ]);
+  // });
+
   it("gets the positions of the given scale", () => {
-    const fb = frets();
-    const ams = scale("A minor");
-    const t = tnps(fb.notes(), ams);
-    assert.equal(t.positions().length, 7);
+    const strs = tnps(frets().notes(), scale("D minor"));
+    // console.log(strs);
+    // const e = strings[0];
+    // const enote = e[0];
+    // const dnote = e[10];
+
+    console.log(strs[0].map((n) => [n.name, n.positions]));
+
+    // assert.equal(enote.name, "E2");
+    // assert.equal(dnote.name, "D3");
+    // assert.equal(dnote.interval, "1P");
+    // assert.deepEqual(enote.positions, [1, 2, 7]);
+    // assert.deepEqual(dnote.positions, [1, 6, 7]);
   });
 });
