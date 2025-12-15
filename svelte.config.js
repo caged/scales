9b8c71d5302f42774f365eb758caae6efc6ebf7c
Justin Palmer <encytemedia@gmail.com>
2025-12-15 16:35:01 -0600

    use built in options for static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -5,13 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-    })
+    adapter: adapter()
   }
 };
 
