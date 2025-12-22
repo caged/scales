381ac31a4dea3e355e27194a9cef3a45ddbe5b08
Justin Palmer <encytemedia@gmail.com>
2025-12-22 08:27:51 -0600

    spike out another page

---
+++ src/routes/scales/[key]/[scale]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { page } from "$app/stores";
+
+  const key = $derived($page.params.key);
+  const scale = $derived($page.params.scale.replace("-", " "));
+</script>
+
+<div>
+  Key: {key} | Scale: {scale}
+</div>
