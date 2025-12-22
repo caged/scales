512ebfd1553748f2be5c44111612306591dcf752
Justin Palmer <encytemedia@gmail.com>
2025-12-22 08:26:54 -0600

    update ScaleInfo to new system

--- src/lib/ScaleInfo.svelte
+++ src/lib/ScaleInfo.svelte
@@ -1,52 +1,49 @@
 <script>
   import { onMount } from "svelte";
   import { scalePoint, range } from "d3";
-  export let scale;
-  export let position = null;
+  import { Note } from "tonal";
 
-  let container;
-  let width;
-  let height;
+  let { scale } = $props();
+
+  let container = $state(null);
+  let width = $state(0);
+  let height = $state(45);
+
   const margin = { top: 0, right: 20, bottom: 20, left: 20 };
-  const dotX = scalePoint().domain(range(scale.notes().length));
-  const startPosition = position;
+  const dotX = $derived(
+    scalePoint()
+      .domain(range(scale.notes.length))
+      .range([margin.left, width - margin.right]),
+  );
+
+  const notes = $derived(scale.notes.map((n) => Note.get(n)));
 
   onMount(() => {
     width = container.clientWidth;
     height = 45;
-    dotX.range([margin.left, width - margin.right]);
   });
 </script>
 
-{#if scale}
-  <div bind:this={container}>
-    {#if width}
-      <svg viewBox="0 0 {width} {height}">
-        {#each scale.notes() as note, i}
-          <g transform="translate({dotX(i)}, {20})">
-            <circle
-              r="12"
-              fill={note.interval === "1P"
-                ? "rgb(50, 50, 50)"
-                : "rgb(87, 45, 146)"}
-            />
-            <text
-              text-anchor="middle"
-              dy="4"
-              font-size="10"
-              class="text-white"
-              fill="currentColor">{note.name}</text
-            >
-            <text
-              text-anchor="middle"
-              dy="25"
-              font-size="10"
-              class="text-black"
-              fill="currentColor">{note.interval}</text
-            >
-          </g>
-        {/each}
-      </svg>
-    {/if}
-  </div>
-{/if}
+<div class="w-full h-full" bind:this={container}>
+  <svg class="h-12" viewBox="0 0 {width} {height}">
+    {#each notes as note, i}
+      <g transform="translate({dotX(i)}, {20})">
+        <circle
+          r="12"
+          class={note.interval === "1P" ? "fill-red-500" : "fill-green-500"} />
+        <text
+          text-anchor="middle"
+          dy="4"
+          font-size="10"
+          class="text-white"
+          fill="currentColor">{note.name}</text>
+        <text
+          text-anchor="middle"
+          dy="25"
+          font-size="10"
+          class="text-black"
+          fill="currentColor">{scale.intervals[i]}</text>
+      </g>
+    {/each}
+  </svg>
+</div>
